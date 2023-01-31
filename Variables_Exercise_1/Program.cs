071b2d3c719948f39f57858529fdd1415801d70a
Brian Shaw <1BrianShaw@gmail.com>
2023-01-31 11:41:26 -0500

    initial

---
+++ Variables_Exercise_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+String name = "Brian Shaw";
+int age = 35;
+char middleinitial = 'J';
+bool isBrianShaw = true;
+double NetWorth = 35.7;
+decimal currentprice = 29.99m;
+
+Console.WriteLine($"Hello everyone my name is {name}");
+Console.WriteLine($"I am {age} years old");
+Console.WriteLine($"So there is no confusion with the World's Stongest man my middle initial is {middleinitial}");
+Console.WriteLine($"We get confused for each other because when asked if I am Brian Shaw I have to say that is {isBrianShaw}");
+Console.WriteLine($"However if you ask my current Net Worth it is only{NetWorth} Dollars");
+Console.WriteLine($"I think the other Brian's is {currentprice} Million");
